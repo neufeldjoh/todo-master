7d2dbe4fbb61c98bcd54d5cd902fea1806c6e458
Johann Neufeld <neufeldj@campus.upb.de>
2016-01-05 11:48:38 +0100

    nochmal jenkins testen

--- web/src/main/java/com/manning/gia/todo/web/ToDoServlet.java
+++ web/src/main/java/com/manning/gia/todo/web/ToDoServlet.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 /**
 	Dokumentation
-	Hallo, erneut
+	Hallo, erneut, nochmal
 */
 public class ToDoServlet extends HttpServlet {
     public static final String FIND_ALL_SERVLET_PATH = "/all";
